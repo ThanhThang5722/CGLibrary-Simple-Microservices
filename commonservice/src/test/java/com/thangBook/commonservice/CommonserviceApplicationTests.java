bb53e3b0bd225190c1de295cf89cf2e4fd750581
ThanhThang5722 <23521427@gm.uit.edu.vn>
2025-07-09 13:33:18 +0700

    fix(folder): Restructure folders

---
+++ commonservice/src/test/java/com/thangBook/commonservice/CommonserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thangBook.commonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
