966f8bbaf37d089229aae45a0e8a34e772b12594
ThanhThang5722 <23521427@gm.uit.edu.vn>
2025-07-09 17:42:45 +0700

    feat(book): Add validate layer for add Book api

--- bookservice/src/main/java/com/thangBook/bookservice/command/model/BookRequestModel.java
+++ bookservice/src/main/java/com/thangBook/bookservice/command/model/BookRequestModel.java
@@ -1,20 +1,24 @@
 package com.thangBook.bookservice.command.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookRequestModel {
     private String id;
 
+    @NotBlank(message = "Name is mandatory")
+    @Size(min = 2, max = 30, message = "Name must be between 2 and 30 characters")
     private String name;
-
+    
+    @NotBlank(message = "Author is madatory")
     private String author;
+
 
     private Boolean isReady;
 }
